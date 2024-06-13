3310f42bee55271b6ad04213fa31218708e79262
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-06-13 23:37:53 +0200

    adding register action and passing register form to template

--- app/controllers/UsersCtrl.class.php
+++ app/controllers/UsersCtrl.class.php
@@ -2,8 +2,26 @@
 
 namespace app\controllers;
 
+use app\forms\RegisterForm;
+
 class UsersCtrl extends BaseCtrl {
+    private RegisterForm $form;
+
+    public function __construct() {
+        $this->form = new RegisterForm();
+    }
+
     public function action_register_view() {
-        $this->generateView( 'register.tpl', [] );
+        $this->generateView( 'register.tpl', [ 'form' => $this->form ] );
+    }
+
+    public function action_register() {
+        $isValidForm = $this->form->loadAndValidateDataFromRequest();
+
+        if ( !$isValidForm ) {
+            $this->generateView( 'register.tpl', [ 'form' => $this->form ] );
+
+            return;
+        }
     }
 }
