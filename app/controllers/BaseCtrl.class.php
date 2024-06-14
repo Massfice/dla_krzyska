96b3665dee7a90ad417280f9b16473c4e73d9cbf
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-06-14 11:57:00 +0200

    added helper functions for assigning user to template and checking if user is logged in

--- app/controllers/BaseCtrl.class.php
+++ app/controllers/BaseCtrl.class.php
@@ -2,18 +2,22 @@
 
 namespace app\controllers;
 
+use app\repositories\RoleRepository;
 use core\App;
 use core\RoleUtils;
+use core\SessionUtils;
 
 // class for having some common utility
 
 class BaseCtrl {
     protected function generateView( string $file, array $variables ) {
         $smarty = App::getSmarty();
+        $user = SessionUtils::loadObject( 'usser', true );
 
         $smarty->assign( 'page_title', 'Cantor App' );
         $smarty->assign( 'page_description', 'Cantor app is awesome' );
-        $smarty->assign( 'is_logged_in', $this->isLoggedIn() );
+        $smarty->assign( 'is_logged_in', $this->isLoggedIn( $user ) );
+        $smarty->assign( 'user', $user );
         foreach ( $variables as $key => $value ) {
             $smarty->assign( $key, $value );
         }
@@ -21,7 +25,21 @@ class BaseCtrl {
         $smarty->display( $file );
     }
 
-    protected function isLoggedIn(): bool {
+    protected function isLoggedIn( $user ): bool {
+        if ( !isset( $user ) ) {
+            return false;
+        }
+
         return RoleUtils::inRole( 'user' ) || RoleUtils::inRole( 'admin' );
+    }
+
+    protected function logIn( $user ) {
+        $roles = RoleRepository::getRoles( $user[ 'iduser' ] );
+
+        foreach ( $roles as $role ) {
+            RoleUtils::addRole( $role );
+        }
+
+        SessionUtils::storeObject( 'user', $user );
     }
 }
