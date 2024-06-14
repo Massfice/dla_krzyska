690ca9c4b30eddfb2dd1d978d0aabeb69802d29f
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-06-14 16:02:57 +0200

    added login form

--- app/forms/LoginForm.class.php
+++ app/forms/LoginForm.class.php
@@ -2,8 +2,40 @@
 
 namespace app\forms;
 
+use app\repositories\UsersRepository;
+use core\App;
+use core\ParamUtils;
+use core\Utils;
+
 class LoginForm {
-    public $login;
+    public $username;
     public $password;
-    public $rolename;
+    public $user;
+
+    public function loadAndValidateDataFromRequest() {
+        $this->username = ParamUtils::getFromRequest( 'username' );
+        $this->password = ParamUtils::getFromRequest( 'password' );
+
+        if ( empty( $this->username ) ) {
+            Utils::addErrorMessage( 'Username is required' );
+        }
+
+        if ( empty ( $this->password ) ) {
+            Utils::addErrorMessage( 'Password is required' );
+        }
+
+        $this->user = UsersRepository::getByUsername( $this->username );
+
+        if ( $this->user == null ) {
+            Utils::addErrorMessage( 'Invalid credentials' );
+        }
+
+        $isValidPassword = password_verify( $this->password, $this->user[ 'password' ] );
+
+        if ( !$isValidPassword ) {
+            Utils::addErrorMessage( 'Invalid credentials' );
+        }
+
+        return !App::getMessages()->isError();
+    }
 }
