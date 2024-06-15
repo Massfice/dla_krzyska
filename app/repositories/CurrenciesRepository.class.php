e3abdf956ecf791dc85103e0a5794234c2665197
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-06-16 00:19:01 +0200

    adding functions for getting currency by code and adding currency

--- app/repositories/CurrenciesRepository.class.php
+++ app/repositories/CurrenciesRepository.class.php
@@ -2,6 +2,7 @@
 
 namespace app\repositories;
 
+use app\forms\CurrencyForm;
 use core\App;
 
 class CurrenciesRepository {
@@ -16,5 +17,36 @@ class CurrenciesRepository {
         ] );
 
         return $currencies;
+    }
+
+    static function getCurrency( $code ) {
+        $medoo = App::getDB();
+
+        $currencies = $medoo->select( 'Currency', [
+            'idcurrency',
+            'currencyname',
+            'currencycode',
+            'price_in_dollars'
+        ], [
+            'currencycode' => $code
+        ] );
+
+        return isset( $currencies[ 0 ] ) ? $currencies[ 0 ] : null;
+    }
+
+    static function addCurrency( CurrencyForm $form ) {
+        $medoo = App::getDB();
+
+        $data = [
+            'currencyname' => $form->name,
+            'currencycode' => $form->code,
+            'price_in_dollars' => $form->price
+        ];
+
+        $medoo->insert( 'Currency', $data );
+
+        $data[ 'idcurrency' ] = $medoo->id();
+
+        return $data;
     }
 }
