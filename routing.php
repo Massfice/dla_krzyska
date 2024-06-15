b7c8032e78ae3c94ef1c8b4078aaf797399c2d33
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-06-15 23:37:42 +0200

    adding currencies_view route

--- routing.php
+++ routing.php
@@ -6,13 +6,15 @@ use core\Utils;
 App::getRouter()->setDefaultRoute( 'home' );
 App::getRouter()->setLoginRoute( 'login_view' );
 
-Utils::addRoute( 'home', 'ConversionCtrl' );
+Utils::addRoute( 'home', 'HomeCtrl' );
 
 Utils::addRoute( 'register_view', 'UsersCtrl' );
 Utils::addRoute( 'register', 'UsersCtrl' );
 Utils::addRoute( 'users_view', 'UsersCtrl', [ 'admin' ] );
 Utils::addRoute( 'add_user_view', 'UsersCtrl', [ 'admin' ] );
 Utils::addRoute( 'add_user', 'UsersCtrl', [ 'admin' ] );
+
+Utils::addRoute( 'currencies_view', 'CurrenciesCtrl', [ 'admin' ] );
 
 Utils::addRoute( 'login_view', 'LoginCtrl' );
 Utils::addRoute( 'login', 'LoginCtrl' );
